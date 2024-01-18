8b2bdc27fe66a7c5f74b567c426667a0d35a7fc7
Tomisin Leshi <tom-lesh@outlook.com>
2024-01-18 11:33:36 +0100

    Build: Prettier and Eslint config template added

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  globals: {
+    React: "readonly",
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "no-unused-vars": [1, { args: "after-used", argsIgnorePattern: "^_" }],
   },
-}
+};
+
+// 0: Turn off rule. Will never bug you about unused variables again
+// 1: Warning.       Turn the rule on as a warning (doesn’t affect exit code)
+// 2: Error.         Turn the rule on as an error (exit code will be 1)
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(
-  document.getElementById('stellar-frame') as HTMLElement
+  document.getElementById("stellar-frame") as HTMLElement
 ).render(
   <React.StrictMode>
     <App />
